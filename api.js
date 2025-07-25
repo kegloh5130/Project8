95f74ff33591eacd660ea4d5b063fb74ef676851
Kodjo Egloh <kodjoegloh@MacBookPro.lan>
2025-07-25 09:23:09 -0400

    commit 5

--- api.js
+++ api.js
@@ -16,12 +16,29 @@ console.log('Hot dog!');
 
 
 
+async function getAccessToken() {
+  const res = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: '4KmCNpLicyCKAhCZ17mSMUMiGnf40oEj',
+      client_secret: 'L83oxPF8Aa2bTJ8B'
+    })
+  });
+  const data = await res.json();
+  console.log('Access Token:', data.access_token);
+  return data.access_token;
+}
+// Generating a new access token
 
-const test = async () => {
+const Barc = async () => {
     await fetch('https://test.api.amadeus.com/v1/shopping/activities/by-square?north=13.023577&west=77.536856&south=12.923210&east=77.642256', {
         method: "GET",
         headers: {
-            "Authorization": "Bearer i7aa42fMvmSObqf8MAhJNWFm8VNZ",
+            "Authorization": "Bearer"+ " " + await getAccessToken(),
         },
         mode: "cors",
         catch: "default"
@@ -36,8 +53,26 @@ const test = async () => {
         }
     }).then(random => {
        console.log(random.data[0].name);
+       console.log(random.data[0].pictures[1]);
+       document.querySelector('#results1 img').src = random.data[0].pictures[0];
+       document.querySelector('#results1 h1').innerText = random.data[0].name;
+        document.querySelector('#results1 p').innerText = random.data[0].description;
+        
+        document.querySelector('#results2 img').src = random.data[1].pictures[0];
+        document.querySelector('#results2 h1').innerText = random.data[1].name;
+        document.querySelector('#results2 p').innerText = random.data[1].description;
+
+        document.querySelector('#results3 img').src = random.data[2].pictures[0];
+        document.querySelector('#results3 h1').innerText = random.data[2].name;
+        document.querySelector('#results3 p').innerText = random.data[2].description;
     });
 }
+
+
+
+
+
+
 
 
 
@@ -61,40 +96,3 @@ const test = async () => {
 //   'https://test.api.amadeus.com/v1/shopping/activities?latitude=12.8797&longitude=121.7740&radius=10' \
 //   -H 'accept: application/vnd.amadeus+json' \
 //   -H 'Authorization: Bearer dKRyVT18FUrI1dd1J3YGkB5vKcTn'
-
-
-
-// const TheData = async (userSearch) => {
-//     try {
-//         const response = await axios.get('https://test.api.amadeus.com/v1/', {
-//             params: {
-//                 s: userSearch
-//             }
-//         });
-//         if (response.data.Error) {
-//             return [];
-//         }
-//         // Return the Search results array
-//         return response.data.Search || [];
-//     } catch (error) {
-//         console.error('API error:', error);
-//         return [];
-//     }
-// };
-
-// async function handleSearch() {
-//     const userInput = document.getElementById('searchInput').value;
-//     const results = await TheData(userInput);
-//     const display = document.getElementById('results');
-//     display.innerHTML = '';
-
-//     if (results && results.length > 0) {
-//         results.forEach(item => {
-//             const div = document.createElement('div');
-//             div.textContent = JSON.stringify(item);
-//             display.appendChild(div);
-//         });
-//     } else {
-//         display.textContent = 'No results found.';
-//     }
-// }
